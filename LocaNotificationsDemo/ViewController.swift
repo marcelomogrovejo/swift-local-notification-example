bbda58753ef7ed0d52d722c18f7b2bf8731f1704
Marcelo Mogrovejo <marcelo@mogromacbook.local>
2017-05-29 21:50:06 -0300

    Initial Commit

---
+++ LocaNotificationsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocaNotificationsDemo
+//
+//  Created by Marcelo Mogrovejo on 5/29/17.
+//  Copyright © 2017 Marcelo Mogrovejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
